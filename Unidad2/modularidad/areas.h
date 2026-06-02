12c726e9e36bf9035471d932f37bd65ee5698b97
YairGil <yairgil@gmail.com>
2026-06-02 17:06:10 -0500

    Subiendo taller eficiente

--- Unidad2/modularidad/areas.h
+++
@@ -1,7 +0,0 @@
-#ifndef AREAS_H
-#define AREAS_H
-
-float areaCirculo(float radio);
-float areaCuadrado(float lado);
-
-#endif
--- Unidad2/modularidad/volumenes.h
+++
@@ -1,7 +0,0 @@
-#ifndef VOLUMENES_H
-#define VOLUMENES_H
-
-float volumenEsfera(float radio);
-float volumenCubo(float lado);
-
-#endif
